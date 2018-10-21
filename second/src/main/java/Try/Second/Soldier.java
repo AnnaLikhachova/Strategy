5e6cb3934719f1ecdbcb5b5d1372f37576243a30
Anna Likhachova <37981486+AnnaLikhachova@users.noreply.github.com>
2018-10-21 15:41:19 +0300

    Update Soldier.java

--- second/src/main/java/Try/Second/Soldier.java
+++ second/src/main/java/Try/Second/Soldier.java
@@ -1,20 +1,18 @@
 package Try.Second;
 
 public class Soldier extends Warrior {
-	String name = "Soldier";
-	int strength = 3;
-	int agility = 1;
-	int inteligence = 1;
-	Play scr;
+	private String name = "Soldier";
+	private int strength = 3;
+	private int agility = 1;
+	private int inteligence = 1;
 	
 	Soldier(){
 		warriorStrength( );
-	
 	}
 
 	@Override
 	public int warriorStrength() {
-		int s = 1+(strength + agility + inteligence);
-		return s;
+		int warriorStrength = 1+(strength + agility + inteligence);
+		return warriorStrength;
 	}
 }
