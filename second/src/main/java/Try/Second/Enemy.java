2cbe4d1fc3c0356eac7eaf30e4f5fa6276ea77ab
Anna Likhachova <37981486+AnnaLikhachova@users.noreply.github.com>
2018-10-21 15:38:36 +0300

    Update Enemy.java

--- second/src/main/java/Try/Second/Enemy.java
+++ second/src/main/java/Try/Second/Enemy.java
@@ -9,14 +9,13 @@ public class Enemy {
 	private int cultureK = 2;
 	private int scienceK = 2;
 	private int religionK = 2;
-	Play sc;
 
 	Enemy(Play scr) {
 		this.sc = scr;		
 	}
 
 	public int enemyStrength() {
-		int st = gold*goldK+culture*cultureK+science*scienceK+religion*religionK;
-		return st;
+		int strengh = gold*goldK+culture*cultureK+science*scienceK+religion*religionK;
+		return strengh;
 	}
 }
